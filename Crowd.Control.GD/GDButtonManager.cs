25dc40b41dfaede6bf7bf8680913077f546c09b7
Noah <altenyt@gmail.com>
2020-03-23 20:13:42 -0400

    remove useless using

--- Crowd.Control.GD/GDButtonManager.cs
+++ Crowd.Control.GD/GDButtonManager.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Alten. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using WindowsInput;
 using WindowsInput.Native;
 
